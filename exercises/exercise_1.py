8040db2098169da310573477429861ae1c53c7c0
ryanoharrow <48161201+ryanoharrow@users.noreply.github.com>
2024-03-25 12:24:31 +0000

    exercise 1

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,9 @@
 # Exercise 1
-# Your solution comes here
+
+number = input()
+12345
+
+first_num = (number % 10) + ((number % 1000) // 100) + (number // 10000)
+second_num = ((number % 100) // 10) + ((number % 10000) // 1000)
+
+print (f"Number {number} becomes {first_num}{second_num}")
